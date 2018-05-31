b8a2770099dec4212c59af9f5c1e0673359402ba
Sebastian Castro Giraldo <sebastiancg1070@gmail.com>
2018-05-30 22:16:39 -0500

    sadas

---
+++ interna.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico in the root directory -->
+        <link type="image/x-icon" href="img/ico.png" rel="icon">
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/jquery.fullpage.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/mainSingle.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+
+    <div id="fullpage">
+        <div class="section" style="background-color:blue;">
+            <div class="theText">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum possimus labore consectetur fugit facere numquam maxime quibusdam praesentium, unde temporibus tenetur laudantium qui a voluptates. Harum est vel amet voluptatum!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum possimus labore consectetur fugit facere numquam maxime quibusdam praesentium, unde temporibus tenetur laudantium qui a voluptates. Harum est vel amet voluptatum!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum possimus labore consectetur fugit facere numquam maxime quibusdam praesentium, unde temporibus tenetur laudantium qui a voluptates. Harum est vel amet voluptatum!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum possimus labore consectetur fugit facere numquam maxime quibusdam praesentium, unde temporibus tenetur laudantium qui a voluptates. Harum est vel amet voluptatum!</p>
+            </div>
+        </div>
+        <div class="section" style="background-color:red;">Some section</div>
+        <div class="section" style="background-color:yellow;">Some section</div>
+    </div>
+           
+ 
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/jquery.fullpage.min.js"></script>
+    <script src="js/jquery.fullpage.extensions.min.js"></script>
+    
+    <!-- scripts main page -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#fullpage').fullpage({
+                //Scrolling
+                scrollOverflow: true,
+                // scrollOverflowReset: true,
+
+                onLeave: function(index, nextIndex, direction){},
+                afterLoad: function(anchorLink, index){},
+                afterRender: function(){},
+                afterResize: function(){},
+                afterResponsive: function(isResponsive){},
+                afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+                onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+            });
+        });
+    </script>
+        
+    </body>
+</html>
