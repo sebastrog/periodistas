244cae0cb20a73a5bfd023a0ca5fc3fcab143152
Your Name <sebastiancg1070@gmail.com>
2018-06-04 21:47:20 -0500

    asda

---
+++ detalles.php
@@ -0,0 +1,321 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>DDHH en el posconflicto colombiano</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico in the root directory -->
+        <link type="image/x-icon" href="img/ico.png" rel="icon">
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/mainSingle.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!-- <div class="">
+            <section class="container-menu-top">
+                <div class="toggleMenu">
+                    <button class="hamburger hamburger--spin" type="button">
+                        <span class="hamburger-box">
+                            <span class="hamburger-inner"></span>
+                        </span>
+                    </button>
+                </div>
+
+                <div class="wrapper-logo-menu">
+                    <img src="img/logo-proyecto-internas.png" alt="">
+                </div>
+
+                <div class="social-media-menu float-right">
+                    <a href="#"><img src="img/twitter-menu.png"></a>
+                    <a href="#"><img src="img/facebook-menu.png"></a>
+                    <a href="#"><img src="img/gplus-menu.png"></a>
+                </div>
+
+
+            </section>
+
+            <div class="white-background-items-menu">
+                <div class="wrapper-items-menu">
+                    <div class="items-menu owl-carousel">
+                        <div class="container-item-menu-report">
+                            <a href="#">
+                                <div class="wrapper-img-menu-report">
+                                    <img src="img/img-post-1.png" alt="">
+                                </div>
+                                <h3>Vulneraciones a los derechos humanos en planadas</h3>
+                            </a>
+                        </div>
+                        <div class="container-item-menu-report">
+                            <a href="#">
+                                <div class="wrapper-img-menu-report">
+                                    <img src="img/img-post-1.png" alt="">
+                                </div>
+                                <h3>Vulneraciones a los derechos humanos en planadas</h3>
+                            </a>
+                        </div>
+                        <div class="container-item-menu-report">
+                            <a href="#">
+                                <div class="wrapper-img-menu-report">
+                                    <img src="img/img-post-1.png" alt="">
+                                </div>
+                                <h3>Vulneraciones a los derechos humanos en planadas</h3>
+                            </a>
+                        </div>
+                        <div class="container-item-menu-report">
+                            <a href="#">
+                                <div class="wrapper-img-menu-report">
+                                    <img src="img/img-post-1.png" alt="">
+                                </div>
+                                <h3>Vulneraciones a los derechos humanos en planadas</h3>
+                            </a>
+                        </div>
+                        <div class="container-item-menu-report">
+                            <a href="#">
+                                <div class="wrapper-img-menu-report">
+                                    <img src="img/img-post-1.png" alt="">
+                                </div>
+                                <h3>Vulneraciones a los derechos humanos en planadas</h3>
+                            </a>
+                        </div>
+                        <div class="container-item-menu-report">
+                            <a href="#">
+                                <div class="wrapper-img-menu-report">
+                                    <img src="img/img-post-1.png" alt="">
+                                </div>
+                                <h3>Vulneraciones a los derechos humanos en planadas</h3>
+                            </a>
+                        </div>
+                        <div class="container-item-menu-report">
+                            <a href="#">
+                                <div class="wrapper-img-menu-report">
+                                    <img src="img/img-post-1.png" alt="">
+                                </div>
+                                <h3>Vulneraciones a los derechos humanos en planadas</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <section class="container-home-main-banner coverVideo" style="background-image:url(img/banner-header-colombia-tiny.png);">
+            <div class="videoContainer">
+                <iframe class="videoContainer__video" width="1920" height="1080" src="http://www.youtube.com/embed/wTcNtgA6gHs?modestbranding=1&autoplay=1&fs=0&loop=1&rel=0&showinfo=0&disablekb=1&playlist=IsBInsOj8TY" frameborder="0"></iframe>
+            </div>
+            <div class="button-play-single-header">
+                <img id="play_boton_youtube" src="img/play-button-video.png">
+            </div>
+            <div class="container">
+                <div class="row wrapper-top-header-logos">
+                    <div class="col-6 icons-logos-external-links">
+                        <a target="_blank" href="https://verdadabierta.com/">
+                            <img src="img/logo-link-10years.png" alt="">
+                        </a>
+                        <a target="_blank" href="https://consejoderedaccion.org/">
+                            <img src="img/logo-link-dr.png" alt="">
+                        </a>
+                    </div>
+                    <div class="col-6 icons-social-media-header">
+                        <a target="_blank" href="http://twitter.com/share?text=DDHH en el posconflicto colombiano&url=https://verdadabierta.com/especiales-v/2018/ddhh-posconflicto-colombiano">
+                            <img src="img/twitter-header.png">
+                        </a>
+                        <a target="_blank" href="http://www.facebook.com/sharer.php?u=https://verdadabierta.com/especiales-v/2018/ddhh-posconflicto-colombiano&title=DDHH en el posconflicto colombiano">
+                            <img src="img/facebook-header.png">
+                        </a>
+                        <a target="_blank" href="https://plus.google.com/share?url=https://verdadabierta.com/especiales-v/2018/ddhh-posconflicto-colombiano">
+                            <img src="img/gplus-header.png">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="container floating-bottom">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="">
+                            <h1><img src="img/logo-proyecto-home.png" alt="DDHH en el posconflicto colombiano"></h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="wrapper-text-home">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <p>
+                            <span class="capital-letter">E</span>ste proyecto es una alianza de Consejo de Redacción y el portal VerdadAbierta.com, con el apoyo del Programa de Derechos Humanos de USAID, orientado a compartir con periodistas y comunicadores de Antioquia, Córdoba, Caquetá y Nariño, diversas herramientas, estrategias y conocimientos útiles para la generación de contenidos responsables y de calidad sobre el abanico de temas que componen los derechos humanos integrales.</br></br>
+                            Los conocimientos adquiridos durante las sesiones de trabajo serán puestos en práctica por un grupo de periodistas seleccionados, quienes, con la asesoría de editores de Consejo de Redacción y de VerdadAbierta.com, desarrollarán una investigación propuesta por ellos sobre un tema de alto impacto en sus comunidades de influencia.</br></br>
+                            Adicionalmente, un equipo de periodistas del portal VerdadAbierta.com tendrá a cargo la producción de tres investigaciones, enfocadas en los departamentos de Tolima, Cauca y Meta, que abordarán temas que para los pobladores de estas regiones son importantes visibilizar por cuanto hacen parte de sus preocupaciones diarias, pero también de sus logros y avances sociales, económicos, culturales, ambientales y políticos.</br></br>
+                            Los reportajes producidos bajo esta alianza serán publicado en este micrositio y divulgados a través del portal Colombiacheck, uno de los aliados de este Proyecto.</br></br>
+                            <!-- Adicionalmente, un equipo de periodistas del portal Verdad Abierta tendrá a cargo la producción de tres investigaciones, enfocadas en los departamentos de Tolima, Cauca y Meta, que abordarán temas que para los pobladores de estas regiones son importantes visibilizar por cuanto hacen parte de sus preocupaciones diarias, pero también de sus logros y avances sociales, económicos, culturales, ambientales y políticos.</br></br> -->
+                            Para conocer más detalles sobre el desarrollo de la primera fase de este proyecto, invitamos a nuestros lectores a dar <a href="https://consejoderedaccion.org/noticias/item/609-periodismo-y-dd-hh-en-las-regiones-de-colombia-cdr-en-mas-de-20-municipios-colombianos" target="_blank">click aquí</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="wrapper-reports-list-home">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-12">
+                                <h2>REPORTAJES</h2>
+                            </div>
+                            <div class="col-12">
+                                <div class="row items-reports">
+                                    <div class="col-12 col-md-6">
+                                        <a href="cafe-aromatiza-procesos-paz.html">
+                                            <div class="container-item-report">
+                                                <div class="wrapper-img-report">
+                                                    <img src="img/Foto-portada-Planadas-Tolima.jpg" alt="">
+                                                </div>
+                                                <h3>Café aromatiza dos procesos de paz</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <!--
+                                    <div class="col-12 col-md-6">
+                                        <a href="#">
+                                            <div class="container-item-report">
+                                                <div class="wrapper-img-report">
+                                                    <img src="img/img-post-2.png" alt="">
+                                                </div>
+                                                <h3>el posconflicto en el sur del tolima</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="col-12 col-md-6">
+                                        <a href="#">
+                                            <div class="container-item-report">
+                                                <div class="wrapper-img-report">
+                                                    <img src="img/img-post-3.png" alt="">
+                                                </div>
+                                                <h3>Indígenas en riesgo en Cauca</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="col-12 col-md-6">
+                                        <a href="#">
+                                            <div class="container-item-report">
+                                                <div class="wrapper-img-report">
+                                                    <img src="img/img-post-4.png" alt="">
+                                                </div>
+                                                <h3>el posconflicto en el sur del tolima</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="col-12 col-md-6">
+                                        <a href="#">
+                                            <div class="container-item-report">
+                                                <div class="wrapper-img-report">
+                                                    <img src="img/img-post-1.png" alt="">
+                                                </div>
+                                                <h3>Vulneraciones a los derechos humanos en planadas</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="col-12 col-md-6">
+                                        <a href="#">
+                                            <div class="container-item-report">
+                                                <div class="wrapper-img-report">
+                                                    <img src="img/img-post-2.png" alt="">
+                                                </div>
+                                                <h3>el posconflicto en el sur del tolima</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="col-12 col-md-6">
+                                        <a href="#">
+                                            <div class="container-item-report">
+                                                <div class="wrapper-img-report">
+                                                    <img src="img/img-post-3.png" alt="">
+                                                </div>
+                                                <h3>el posconflicto en el sur del tolima</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="lastsection" style="background-image:url(img/background-footer-inter.png);">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="quote40">
+                            <p>Este micrositio fue posible gracias al apoyo del pueblo Americano y el gobierno de Estados Unidos, a través de su Agencia para el Desarrollo Internacional (USAID). Los contenidos de los trabajos periodísticos que aquí se publican son responsabilidad exclusiva de sus autores y no necesariamente reflejan los puntos de vista de USAID ni del gobierno de los Estados Unidos.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer class="nottopfoter">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="left-footer">
+                            <h4>Un proyecto de</h4>
+                            <div class="container-logos-left-footer">
+                                <div class="item-logo-footer">
+                                    <a target="_blank" href="https://verdadabierta.com/"><img class="img-responsive" src="img/logo-footer-10years.png"></a>
+                                </div>
+                                <div class="item-logo-footer middle-logo">
+                                    <a target="_blank" href="https://consejoderedaccion.org/"><img class="img-responsive" src="img/logo-footer-dr.png"></a>
+                                </div>
+                                <div class="item-logo-footer">
+                                    <a target="_blank" href="https://colombiacheck.com/"><img class="img-responsive" src="img/logo-footer-colombiacheck.png"></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="right-footer">
+                            <h4>Con el apoyo de</h4>
+                            <div class="container-logos-right-footer">
+                                <div class="item-logo-footer">
+                                    <a href="#"><img src="img/logo-footer-javeriana.png"></a>
+                                </div>
+                                <div class="item-logo-footer">
+                                    <a href="#"><img src="img/logo-footer-usaid.png"></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bottom-footer float-right">
+                            <span>Hecho por </span>
+                            <a target="_blank" href="https://digitalcocktail.co"><img src="img/logo-digital-cocktail-horizontal-gris.png"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- scripts main page -->
+    <script type="text/javascript">
+        $(".button-play-single-header").on("click", function(){
+            $(this).fadeOut();
+            $(".videoContainer").fadeIn();
+        });
+    </script>
+
+    </body>
+</html>
+<script type="text/javascript"></script>
